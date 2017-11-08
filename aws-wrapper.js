6d65c8fa6d0c7771b95a90cfb7f6161a7c796398
Julius <julius@cloud.house>
2017-11-08 01:18:27 +0000

    Remove ssm init

--- aws-wrapper.js
+++ aws-wrapper.js
@@ -1,8 +1,6 @@
 
 const aws = require( 'aws-sdk' )
 
-const ssm = new aws.SSM({apiVersion: '2014-11-06'})
-
 module.exports = {
     getSSMValue: ( key, region = process.env.AWS_REGION ) => {
         const ssm = new aws.SSM({ apiVersion: '2014-11-06', region: region });
