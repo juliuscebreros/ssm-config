fcc3158c1e0c3941e13ead65e587f6e7e1fda8d0
Julius <julius@cloud.house>
2017-11-08 01:03:40 +0000

    Pass region as optional parameter

--- aws-wrapper.js
+++ aws-wrapper.js
@@ -4,7 +4,8 @@ const aws = require( 'aws-sdk' )
 const ssm = new aws.SSM({apiVersion: '2014-11-06'})
 
 module.exports = {
-    getSSMValue: ( key ) => {
+    getSSMValue: ( key, region = process.env.AWS_REGION ) => {
+        const ssm = new aws.SSM({ apiVersion: '2014-11-06', region: region });
         return ssm.getParameter({
             Name: key
         } ).promise().then( ( res ) => {
