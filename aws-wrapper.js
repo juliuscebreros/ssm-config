ad0bbca21e42b2df8c6ef477866d574cfdd0ac0c
Julius <julius@cloud.house>
2017-11-08 02:36:28 +0000

    Descript keys by default

--- aws-wrapper.js
+++ aws-wrapper.js
@@ -5,7 +5,8 @@ module.exports = {
     getSSMValue: ( key, region ) => {
         const ssm = new aws.SSM({ apiVersion: '2014-11-06', region: region });
         return ssm.getParameter({
-            Name: key
+            Name: key,
+            WithDecryption: true
         } ).promise().then( ( res ) => {
             return res.Parameter.Value;
         } );
